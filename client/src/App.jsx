60aec4a319a9886c8ddb9100e53e01399a154af8
Joywinbarboza <joywinbarboza420@gmail.com>
2024-03-16 12:51:56 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Header from "./layouts/Header";
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
